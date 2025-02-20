ad3e5675fdb61ee19b7802d861c8a9a3153fb513
Muhammadamin <abdullayevmuhammadamin89@example.com>
2025-02-20 18:56:32 +0500

    Initial commit

---
+++ src/main/java/org/example/deliveryservicebot/DeliveryServiceBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.deliveryservicebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/deliveryservicebot/DeliveryServiceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deliveryservicebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
